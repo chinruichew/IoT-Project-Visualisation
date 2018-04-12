3a625d646847afefb200eb41a41b532db3d2910a
Chin Rui <chewchinrui@gmail.com>
2018-04-12 18:43:09 +0800

    Refactored App

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.static('data'));
+
+app.get('/', async(req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/api/getSlopeData', async(req, res) => {
+    fs.readFile('./data/slopes.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        res.send(JSON.parse(data));
+    });
+});
+
+app.listen(5000);
